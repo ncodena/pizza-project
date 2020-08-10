4ffe0cddf12866e8a0a91d31db41c42377e0accd
Nuria <c.odena.nuria@gmail.com>
2020-08-10 17:10:01 +0200

    connecting to MyQL Database

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
+// Connect to database
 
+$conn = mysqli_connect('localhost', 'nuria', 'test1234', 'pizza_mania');
+
+// Checking connection
+
+if(!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
+}
 
 
 ?>
