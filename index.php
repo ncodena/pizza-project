88f8de6f793e4ef197c32c2b9411616d2796e288
Nuria <c.odena.nuria@gmail.com>
2020-08-10 17:50:33 +0200

    Control Flow Alt Syntax

--- index.php
+++ index.php
@@ -46,16 +46,16 @@ mysqli_close($conn);
 <h4 class="center grey-text">OUR SELECTION</h4>
 <div class="container">
     <div class="row">
-        <?php foreach($pizzas as $pizza) { ?>
+        <?php foreach($pizzas as $pizza) : ?>
 
         <div class="col s6 md3">
             <div class="card z-depth-0">
                 <div class="card-content center">
                     <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
                     <ul>
-                        <?php foreach(explode(',', $pizza['ingredients']) as $ingredient) {?>
+                        <?php foreach(explode(',', $pizza['ingredients']) as $ingredient): ?>
                         <li><?php echo htmlspecialchars($ingredient) ?></li>
-                        <?php }  ?>
+                        <?php endforeach;  ?>
                     </ul>
                 </div>
                 <div class="card-action right-align">
@@ -63,7 +63,13 @@ mysqli_close($conn);
                 </div>
             </div>
         </div>
-        <?php } ?>
+        <?php endforeach; ?>
+
+        <?php if(count($pizzas) >= 2): ?>
+            <p>There are two or more pizzas</p>
+        <?php else : ?>
+        <p>There are less than two pizzas</p>
+        <?php endif; ?>
 
 
     </div>
