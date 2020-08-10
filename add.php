3fc0f6b618151a656e7c3980e1457c8169d95708
Nuria <c.odena.nuria@gmail.com>
2020-08-10 15:32:25 +0200

    basic form validation

--- add.php
+++ add.php
@@ -1,15 +1,29 @@
 <?php
 
-// if(isset($_GET['submit'])){
-//     echo $_GET['email'];
-//     echo $_GET['title'];
-//     echo $_GET['ingredients'];
-// }
-
 if(isset($_POST['submit'])){
-    echo htmlspecialchars($_POST['email']);
-    echo htmlspecialchars($_POST['title']);
-    echo htmlspecialchars($_POST['ingredients']);
+
+    // check email
+    if(empty($_POST['email'])){
+        echo 'An email is required <br />';
+    } else {
+        echo htmlspecialchars($_POST['email']);
+    }
+
+    // Check title
+
+    if(empty($_POST['title'])){
+        echo 'A title is required <br />';
+    } else {
+        echo htmlspecialchars($_POST['title']);
+    }
+
+    // Check ingredients
+
+    if(empty($_POST['ingredients'])){
+        echo 'At least one ingredient is required <br />';
+    } else {
+        echo htmlspecialchars($_POST['ingredients']);
+    } 
 }
 
 
