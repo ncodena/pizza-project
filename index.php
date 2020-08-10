01f657f06baf97ff89f409ee8ad11ff5293129d2
Nuria <c.odena.nuria@gmail.com>
2020-08-10 17:20:46 +0200

    Getting Data From the Database

--- index.php
+++ index.php
@@ -10,6 +10,21 @@ if(!$conn){
     echo 'Connection error: ' . mysqli_connect_error();
 }
 
+// Write query for all pizzas
+
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+// Make query & get result
+
+$result = mysqli_query($conn, $sql);
+
+// Fetch the resulting rows as an array
+
+
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+print_r($pizzas);
+
 
 ?>
 
