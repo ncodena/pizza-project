242610acbe89b2fc587351eed6931933576a09e9
Nuria <c.odena.nuria@gmail.com>
2020-08-11 13:50:51 +0200

    settings

--- details.php
+++ details.php
@@ -49,7 +49,7 @@ if(isset($_GET['id'])){
 
 <?php include('templates/header.php'); ?>
 
-<div class="container center">
+<div class="container center grey-text">
 
     <?php if($pizza):  ?>
     <h4><?php echo htmlspecialchars($pizza['title']);?></h4>
--- index.php
+++ index.php
@@ -42,6 +42,7 @@ mysqli_close($conn);
 
         <div class="col s6 md3">
             <div class="card z-depth-0">
+                <img src="img/pizza.svg" class="pizza" alt="pizza">
                 <div class="card-content center">
                     <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
                     <ul>
