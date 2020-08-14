84a0c3bba9cdf5c891e9a50eb2e0702fb897f938
Nuria <c.odena.nuria@gmail.com>
2020-08-14 22:03:41 +0200

    setting cookies

--- templates/header.php
+++ templates/header.php
@@ -49,7 +49,7 @@
     <div class="container">
       <a href="index.php" class="brand-logo brand-text">Pizza Mania</a>
       <ul id="nav-mobile" class="right hide-on-small-and-down">
-        <li class="grey-text">Helloo <?php echo htmlspecialchars($name); ?></li>
+        <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
         <li class="grey-text">(<?php echo htmlspecialchars($gender); ?>)</li>
         <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
       </ul>
